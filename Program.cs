986ddec157f3b0cb2b811a6bff773a1017713d56
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-12 07:55:39 +0900

    設定ファイル（JSON）関係

--- Program.cs
+++ Program.cs
@@ -4,9 +4,20 @@ namespace MasterDataGenerator
 {
 	class Program
 	{
+		/// <summary>
+		/// 設定ファイルパス
+		/// </summary>
+		private static readonly string SettingFilePath = "./Setting.json";
+		
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var settings = SettingFile.Load(SettingFilePath);
+			if (settings == null)
+			{
+				Console.WriteLine("設定ファイルが無かったので新規作成します");
+				settings = new SettingFile();
+				settings.Save(SettingFilePath);
+			}
 		}
 	}
 }
---
+++ SettingFile.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace MasterDataGenerator
+{
+	/// <summary>
+	/// 設定ファイル
+	/// </summary>
+	[DataContract]
+	public class SettingFile
+	{
+		/// <summary>
+		/// ソースファイルの保存先
+		/// </summary>
+		[DataMember]
+		public string SourcePath { get; set; }
+
+		/// <summary>
+		/// バイナリファイルの保存先
+		/// </summary>
+		[DataMember]
+		public string BinaryPath { get; set; }
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="sourcePath">ソースファイルの保存先</param>
+		/// <param name="binaryPath">バイナリファイルの保存先</param>
+		public SettingFile(string sourcePath = "out/sources", string binaryPath = "out/binaries")
+		{
+			SourcePath = sourcePath;
+			BinaryPath = binaryPath;
+		}
+
+		/// <summary>
+		/// 読み込み
+		/// </summary>
+		/// <param name="filePath">ファイルパス</param>
+		public static SettingFile Load(string filePath)
+		{
+			if (!File.Exists(filePath)) { return null; }
+
+			var jsonText = File.ReadAllText(filePath);
+			var serializer = new DataContractJsonSerializer(typeof(SettingFile));
+			SettingFile setting = null;
+			using (var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonText)))
+			{
+				memoryStream.Seek(0, SeekOrigin.Begin);
+				setting = serializer.ReadObject(memoryStream) as SettingFile;
+			}
+			return setting;
+		}
+
+		/// <summary>
+		/// 保存
+		/// </summary>
+		/// <param name="filePath">ファイルパス</param>
+		public void Save(string filePath)
+		{
+			var serializer = new DataContractJsonSerializer(typeof(SettingFile));
+			using (StreamWriter writer = new StreamWriter(filePath))
+			{
+				serializer.WriteObject(writer.BaseStream, this);
+			}
+		}
+	}
+}
